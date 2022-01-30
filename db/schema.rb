b93b3dbe93ca88d06261a165d8ea7b951b40d164
regiscarlos13@gmail.com <regis.nascimento@ciccr-ce.interno>
2022-01-30 09:36:40 -0300

    Create Product

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,50 +12,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_120805) do
-
+ActiveRecord::Schema.define(version: 20_220_130_122_603) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "system_requirements", force: :cascade do |t|
-    t.string "name"
-    t.string "operational_system"
-    t.string "storage"
-    t.string "processor"
-    t.string "memory"
-    t.string "video_board"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'products', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.decimal 'price', precision: 10, scale: 2
+    t.string 'productable_type', null: false
+    t.bigint 'productable_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[productable_type productable_id], name: 'index_products_on_productable'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at", precision: 6
-    t.string "confirmation_token"
-    t.datetime "confirmed_at", precision: 6
-    t.datetime "confirmation_sent_at", precision: 6
-    t.string "unconfirmed_email"
-    t.string "name"
-    t.string "email"
-    t.integer "profile", default: 1
-    t.json "tokens"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  create_table 'system_requirements', force: :cascade do |t|
+    t.string 'name'
+    t.string 'operational_system'
+    t.string 'storage'
+    t.string 'processor'
+    t.string 'memory'
+    t.string 'video_board'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
+  create_table 'users', force: :cascade do |t|
+    t.string 'provider', default: 'email', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at', precision: 6
+    t.boolean 'allow_password_change', default: false
+    t.datetime 'remember_created_at', precision: 6
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at', precision: 6
+    t.datetime 'confirmation_sent_at', precision: 6
+    t.string 'unconfirmed_email'
+    t.string 'name'
+    t.string 'email'
+    t.integer 'profile', default: 1
+    t.json 'tokens'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index %w[uid provider], name: 'index_users_on_uid_and_provider', unique: true
+  end
 end
---
+++ spec/factories/products.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :product do
+    sequence(:name) { |n| "Product #{n}" }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 100.0..400.0) }
+  end
+end

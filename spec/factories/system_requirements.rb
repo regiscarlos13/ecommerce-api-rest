bf8dfd24e60fe72904c40b42bab7c2afeabc6871
regiscarlos13@gmail.com <regis.nascimento@ciccr-ce.interno>
2022-01-30 09:20:01 -0300

    Model Category/SystemRequiment

---
+++ spec/factories/system_requirements.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :system_requirement do
+    sequence(:name) { |n| "Basic #{n}" }
+    operational_system { Faker::Computer.os }
+    storage { '5GB' }
+    processor { 'AMD Ryzen 7' }
+    memory { '2GB' }
+    video_board { 'N/A' }
+  end
+end
